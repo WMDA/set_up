50bab84ca5afc2c7d140bac70f0ba2ec2b6ea9fd
Daniel Halls <daniel.halls@nottingham.ac.uk>
2024-05-01 16:59:30 +0100

    wrote setting up script for mac

--- mac/mac_set_up.sh
+++ mac/mac_set_up.sh
@@ -6,5 +6,24 @@ echo "Installing homebrew"
 echo "Installing ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Installing tmux"
+echo "Installing tmux and setting up"
 brew install tmux
+cp ../tmux.conf $HOME/.tmux.conf
+echo "if [ "$TMUX" = "" ]; then exec tmux; fi" >> $HOME/.zshrc
+
+
+echo "Installing bat"
+brew install bat
+
+echo "Installing eza"
+brew install eza
+
+echo "Setting up .zshrc"
+echo 'alias cat="bat --paging=never"' >> $HOME/.zshrc
+echo 'alias ls="eza -l"' >> $HOME/.zshrc
+echo 'alias bcat='/bin/cat'' >> $HOME/.zshrc
+echo 'alias python="python3"' >> $HOME/.zshrc
+
+
+echo "finished"
+source $HOME/.zshrc
